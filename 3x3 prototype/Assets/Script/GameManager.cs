b2cb4a8000ef4fb571820ec01ddc3e1b15b56dd8
acas1028 <63082364+acas1028@users.noreply.github.com>
2021-01-04 12:03:13 +0900

    프로토타입
    프로토타입입니다.

---
+++ 3x3 prototype/Assets/Script/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private static GameManager Instance;
+    private string userName;
+    public float musicVol = 0.5f;
+    public float soundVol = 0.5f;
+    public static GameManager instance
+    {
+        get
+        {
+            if(Instance == null)
+            {
+                var obj = FindObjectOfType<GameManager>();
+                if(obj != null)
+                {
+                    Instance = obj;
+                }
+                else
+                {
+                    var newSingleton = new GameObject("GameManager").AddComponent<GameManager>();
+                    Instance = newSingleton;
+                }
+            }
+            return Instance;
+        }
+        private set
+        {
+            Instance = value;
+        }
+    }
+
+    void Awake()
+    {
+        var objs = FindObjectsOfType<GameManager>();
+        if(objs.Length!= 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        userName = "";
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        if(PlayerPrefs.HasKey("Name"))
+        {
+            userName = PlayerPrefs.GetString("Name");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void setUserName(string pUserName)
+    {
+        userName = pUserName;
+    }
+
+    public string getUserName()
+    {
+        return userName;
+    }
+}
---
+++ 3x3 prototype/Assets/Script/Prologue/PrologueChange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PrologueChange : MonoBehaviour
+{
+    public GameObject currentButton;
+    public GameObject nextButton;
+    public int sceneNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeImage()
+    {
+        currentButton.SetActive(false);
+        nextButton.SetActive(true);
+
+        if (nextButton.transform.tag == "Finish")
+            SceneManager.LoadScene(sceneNumber);
+
+    }
+}
