87d2fd4b0c1e2fd3e2bb86167dc3cb3088758848
acas1028 <63082364+acas1028@users.noreply.github.com>
2021-01-05 15:59:09 +0900

    2021/01/05 인벤토리 경합
    인벤토리 이동과 onoff 구현

---
+++ 3x3 prototype/Assets/Script/MainGame/MainScripts/ExampleStep.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExampleStep : MonoBehaviour
+{
+    public GameObject Object;
+    public Text text;
+    int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count = Object.GetComponent<CompareActiveAnswer>().getingameStep();
+        text.text = "단계:" + count;
+    }
+}
